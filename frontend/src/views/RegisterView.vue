e4cb6fb1c85df33f12ca568731f55e2db6f8f0d0
Vikaash Harichandran <jbvikaash@gmail.com>
2024-12-05 18:05:46 +0800

    Create RegisterView.vue

---
+++ frontend/src/views/RegisterView.vue
@@ -0,0 +1,16 @@
+<!-- src/views/RegisterView.vue -->
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <RegisterForm />
+  </div>
+</template>
+
+<script>
+import RegisterForm from '@/components/RegisterForm.vue'
+
+export default {
+  components: {
+    RegisterForm
+  }
+}
+</script>
