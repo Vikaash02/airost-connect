2543eeb131f943b4d7e187e4f3654c2b45dc0ded
Vikaash Harichandran <jbvikaash@gmail.com>
2024-12-05 18:06:38 +0800

    Create ProgramRecommendationView.vue

---
+++ frontend/src/views/ProgramRecommendationView.vue
@@ -0,0 +1,14 @@
+<!-- src/views/ProgramRecommendationView.vue -->
+<template>
+  <ProgramList />
+</template>
+
+<script>
+import ProgramList from '@/components/ProgramList.vue'
+
+export default {
+  components: {
+    ProgramList
+  }
+}
+</script>
