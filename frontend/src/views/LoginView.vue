aa82d0afd0a447a46cf1b82b38a164ecec57d171
Vikaash Harichandran <jbvikaash@gmail.com>
2024-12-05 18:05:29 +0800

    Create LoginView.vue

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,16 @@
+<!-- src/views/LoginView.vue -->
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <LoginForm />
+  </div>
+</template>
+
+<script>
+import LoginForm from '@/components/LoginForm.vue'
+
+export default {
+  components: {
+    LoginForm
+  }
+}
+</script>
