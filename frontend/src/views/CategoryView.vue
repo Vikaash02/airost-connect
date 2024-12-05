d82c6914897f73568b006c4cf6987a17aff883da
Vikaash Harichandran <jbvikaash@gmail.com>
2024-12-05 18:06:17 +0800

    Create CategoryView.vue

---
+++ frontend/src/views/CategoryView.vue
@@ -0,0 +1,14 @@
+<!-- src/views/CategoryView.vue -->
+<template>
+  <CategorySelection />
+</template>
+
+<script>
+import CategorySelection from '@/components/CategorySelection.vue'
+
+export default {
+  components: {
+    CategorySelection
+  }
+}
+</script>
