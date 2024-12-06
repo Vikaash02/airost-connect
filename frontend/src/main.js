08bff58ae020f755f50c90ae24c1cac4ce949e95
Vikaash Harichandran <jbvikaash@gmail.com>
2024-12-06 17:15:21 +0800

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,9 @@
+// main.js
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './assets/tailwind.css'
-
-// Dispatch the action to initialize the default admin user if not exists
-store.dispatch('initializeDefaultAdmin')
 
 createApp(App)
   .use(store)
